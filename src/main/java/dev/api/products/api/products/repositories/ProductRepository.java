fab04e6a71be00bd75d279975fce9d4abc5dd099
matheuslims <omatheusdasilvalima@gmail.com>
2025-03-20 14:56:44 -0300

    Criando o repositorio ProductRepository

---
+++ src/main/java/dev/api/products/api/products/repositories/ProductRepository.java
@@ -0,0 +1,11 @@
+package dev.api.products.api.products.repositories;
+
+import dev.api.products.api.products.models.ProductModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ProductRepository extends JpaRepository<ProductModel, UUID> {
+}
