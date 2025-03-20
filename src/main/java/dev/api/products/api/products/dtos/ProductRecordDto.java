8dabd9c4611fe3b4c1ff0e1a3b188add45116b35
matheuslims <omatheusdasilvalima@gmail.com>
2025-03-20 15:04:59 -0300

    Mapeando o DTO no ProductRecordDto

---
+++ src/main/java/dev/api/products/api/products/dtos/ProductRecordDto.java
@@ -0,0 +1,9 @@
+package dev.api.products.api.products.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.math.BigDecimal;
+
+public record ProductRecordDto(@NotBlank String name, @NotNull BigDecimal value) {
+}
